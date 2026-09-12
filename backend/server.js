524bb9a871aa4ce8248dba0db033ade9e06b0242
Zelan Store Admin <zomuansangajacob523@gmail.com>
2026-09-13 00:53:52 +0530

    Increase body parser payload limit to 50mb

--- backend/server.js
+++ backend/server.js
@@ -74,9 +74,9 @@ app.use(
     },
   })
 );
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static("build"));
